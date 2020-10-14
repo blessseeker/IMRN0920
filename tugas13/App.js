f3603b0e53c7e55fabfd4a5baff2eb285b4cf4d9
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-10-15 06:35:16 +0700

    tugas 13

---
+++ tugas13/App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+import Login from './app/screens/Login';
+import About from './app/screens/About';
+import Daftar from './app/screens/Daftar';
+
+const App = () => {
+  const state = 'register';
+  return (
+    <>
+      <View style={styles.container}>
+        {state == 'isNotLoggedIn' && <Login />}
+        {state == 'register' && <Daftar />}
+        {state == 'isLoggedIn' && <About />}
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
