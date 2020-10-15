9de6f85cc7107a78c84fe376a4a6ec94efb633e7
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-10-15 16:33:04 +0700

    tugas 14

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -12,6 +12,7 @@ import {View, StyleSheet} from 'react-native';
 import Login from './app/screens/Login';
 import About from './app/screens/About';
 import Daftar from './app/screens/Daftar';
+import Skill from './app/screens/Skill';
 
 const App = () => {
   const state = 'isLoggedIn';
@@ -20,7 +21,7 @@ const App = () => {
       <View style={styles.container}>
         {state == 'isNotLoggedIn' && <Login />}
         {state == 'register' && <Daftar />}
-        {state == 'isLoggedIn' && <About />}
+        {state == 'isLoggedIn' && <Skill />}
       </View>
     </>
   );
